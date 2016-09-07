ebd45d9be720b3b1a54c1ca2eed4889d644a3990
AndreasLc1103 <darkninjawolf1@gmail.com>
2016-09-07 10:34:04 -0400

    for some reason it did not commit correctly

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TestBotMap.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TestBotMap.java
@@ -2,13 +2,19 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.modernrobotics.ModernRoboticsUsbUtil;
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsUsbDcMotorController;
-import com.qualcomm.hardware.modernrobotics.
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsUsbServoController;
+import com.qualcomm.hardware.modernrobotics.PretendModernRoboticsUsbDevice;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsDigitalTouchSensor;
+import com.qualcomm.hardware.matrix.MatrixDcMotorController;
+import com.qualcomm.hardware.matrix.MatrixServoController;
+import com.qualcomm.hardware.matrix.MatrixMasterController;
 /**
  * Created by Andreaslc1103 on 9/6/2016.
  */
 public class TestBotMap {
 
 
+//bull shit comment
 
     
 }
