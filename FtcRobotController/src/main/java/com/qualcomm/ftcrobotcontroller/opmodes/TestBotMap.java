6e8d128e5e68ce6cb829127749c59f9d33edf5d6
AndreasLc1103 <darkninjawolf1@gmail.com>
2016-09-08 23:51:59 -0400

    Fixed Errors within the Test Bot map class

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TestBotMap.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TestBotMap.java
@@ -2,9 +2,10 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import android.graphics.Path;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
@@ -13,16 +14,28 @@ import com.qualcomm.robotcore.hardware.DeviceManager;
 /**
  * Created by Andreaslc1103 on 9/6/2016.
  */
-public class TestBotMap extends OpMode{
+public abstract class TestBotMap extends OpMode {
+
 
     DcMotor Right;
     DcMotor Left;
-    public void OpMode() {
 
-        Right = hardwareMap.dcMotor.get("Right Motor");
+    @Override
+    public void init_loop() {
+
+    Right = hardwareMap.dcMotor.get("Right Motor");
         Left = hardwareMap.dcMotor.get("Left Motor");
 
 
         Right.setPower(0);
         Left.setPower(9);
+
+
+
     }
+
+    @Override
+    public void loop() {
+
+    }
+}
